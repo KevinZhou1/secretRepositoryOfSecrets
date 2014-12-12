9f69ead3b0598de495288052cb172e256b4a9c68
Maggie White <mawhite6@wisc.edu>
2014-12-12 15:57:55 -0600

    Created task file and initialized some values

--- DSO_dig_tb.v
+++ DSO_dig_tb.v
@@ -16,20 +16,7 @@ wire cmd_sent,resp_rdy;							// outputs from master UART
 wire [7:0] resp_rcv;
 wire [7:0] ch1_data,ch2_data,ch3_data;
 wire trig1,trig2;
-
-///////////////////////////
-// Define command bytes //
-/////////////////////////
-localparam DUMP_CH  = 8'h01;		// Channel to dump specified in low 2-bits of second byte
-localparam CFG_GAIN = 8'h02;		// Gain setting in bits [4:2], and channel in [1:0] of 2nd byte
-localparam TRIG_LVL = 8'h03;		// Set trigger level, lower byte specifies value (46,201) is valid
-localparam TRIG_POS = 8'h04;		// Set the trigger position. This is a 13-bit number, samples after capture
-localparam SET_DEC  = 8'h05;		// Set decimator, lower nibble of 3rd byte. 2^this value is decimator
-localparam TRIG_CFG = 8'h06;		// Write trig config.  2nd byte 00dettcc.  d=done, e=edge,
-localparam TRIG_RD  = 8'h07;		// Read trig config register
-localparam EEP_WRT  = 8'h08;		// Write calibration EEP, 2nd byte is address, 3rd byte is data
-localparam EEP_RD   = 8'h09;		// Read calibration EEP, 2nd byte specifies address
-
+`include "task.sv"
 //////////////////////
 // Instantiate DUT //
 ////////////////////
@@ -60,62 +47,6 @@ initial clk = 0;
 always
     #2 clk = ~clk; // 500 MHz clock
 
-task gen_init;
-    begin
-    rst_n = 1'b0;
-    repeat(2) @(posedge clk);
-    rst_n = 1'b1;
-    end
-endtask
-
-task init_UART_comm_mstr;
-    begin
-    cmd_snd = 24'h000000;
-    send_cmd = 1'b0;
-    clr_resp_rdy = 1'b1;
-    repeat(2) @(posedge clk);
-    end
-endtask
-
-task send_UART_mstr_cmd;
-    input [23:0] temp_cmd;
-    begin
-    cmd_snd = temp_cmd;
-    send_cmd = 1'b1;
-    clr_resp_rdy = 1'b0;
-    @(posedge clk);
-    send_cmd = 1'b0;
-    end
-endtask
-
-task check_UART_pos_ack;
-    input resp_rdy;
-    input resp;
-    output clr_resp_rdy;
-    begin
-    @(posedge resp_rdy);
-    if(resp === 8'hEE)
-        $display("DIG UART sent a neg ack :(\n");
-    else if(resp !== 8'hA5)
-        $display("DIG UART didn't output anything\n");
-    clr_resp_rdy = 1'b1;
-    @(posedge clk);
-    clr_resp_rdy = 1'b0;
-    if(resp_rdy !== 1'b0)
-        $display("DIG UART resp_rdy didn't clear");
-    end
-endtask
-
-task check_trig_cfg;
-    input [7:0] correct_trig_cfg;
-    input [7:0] cur_trig_cfg;
-    begin
-    if(correct_trig_cfg !== cur_trig_cfg)
-        $display("Expected trig_cfg %d and actual %d do not match",
-                 correct_trig_cfg, cur_trig_cfg);
-    end
-endtask
-
 reg [7:0] trig_cfg;
 reg [7:0] AFE_data;
 reg [2:0] ggg; // Analog gain value
@@ -133,29 +64,33 @@ initial begin
     gen_init();
     init_UART_comm_mstr();
     // Check analog gain configure (cmd 02)
-    send_UART_mstr_cmd({CFG_GAIN, 3'h0, ggg, cc});
-    check_UART_pos_ack(resp_rdy, resp_rcv, clr_resp_rdy);
+    ggg = 3'b000; // analog gain value
+    cc = 2'b00; // channel select
+    send_cfg_gain_cmd(ggg, cc, 1'b1);
     // Check set trigger level     (cmd 03)
-    send_UART_mstr_cmd({TRIG_LVL, 8'hxx, LL});
-    check_UART_pos_ack(resp_rdy, resp_rcv, clr_resp_rdy);
+    LL = 8'h00; // trigger level
+    send_trig_lvl_cmd(LL, 1'b1);
     // Check write trigger position register (cmd 04)
-    send_UART_mstr_cmd({TRIG_POS, 7'h00, ULL});
-    check_UART_pos_ack(resp_rdy, resp_rcv, clr_resp_rdy);
+    ULL = 9'h100; // trigger position
+    send_trig_pos_cmd(ULL, 1'b1);
     // Check set decimator (cmd 05)
-    send_UART_mstr_cmd({SET_DEC, 8'hxx, 4'h0, L});
-    check_UART_pos_ack(resp_rdy, resp_rcv, clr_resp_rdy);
+    L = 4'h0; // decimator
+    send_set_dec_cmd(LL, 1'b1);
     // Check TRIG_CFG
-    send_UART_mstr_cmd({TRIG_CFG, d, e, tt, cc, 8'hxx});
-    check_UART_pos_ack(resp_rdy, resp_rcv, clr_resp_rdy);
+    d = 1'b0; // capture_done
+    e = 1'b1; // edge type, 1 == positive edge, 0 == negative edge
+    tt = 2'b01; // trigger type, 10 = auto roll, 01 = normal, 00 = off
+    cc = 2'b00; // channel select, 00 = channel 1, 01 = channel
+    send_trig_cfg_cmd(d, e, tt, cc, 1'b1);
     // Read TRIG_CFG
-    send_UART_mstr_cmd({TRIG_RD, 16'hxxxx});
-    check_trig_cfg(resp_rcv, {d, e, tt, cc});
+    send_rd_trig_cfg_cmd(d, e, tt, cc);
     // Write calibration EEP
-    send_UART_mstr_cmd({EEP_WRT, 2'h0, aaaaaa, VV});
-    check_UART_pos_ack(resp_rdy, resp_rcv, clr_resp_rdy);
+    aaaaaa = 6'h00; // calibration address
+    VV = 8'h00; // EEPROM calibration data
+    send_eep_wrt_cmd(aaaaaa, VV, 1'b1);
     // Read calibration EEP
-    send_UART_mstr_cmd({EEP_RD, 2'h0, aaaaaa, 8'hxx});
-    // Check EEP read is correct
+    aaaaaa = 6'h00; // calibration address
+    send_eep_rd_cmd(aaaaaa, VV);
     // Check dump channel
     send_UART_mstr_cmd({DUMP_CH, 16'h0000});
 
